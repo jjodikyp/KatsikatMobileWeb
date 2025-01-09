66a50b8051cd8599be223a9134759ed399551191
ImFaldi <rifaldi.indrajaya@binus.ac.id>
2025-01-09 13:55:25 +0700

    merged

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
-const sequelize = require('./src/config/database');
-const routes = require('./src/routes');
+const sequelize = require("./src/config/database");
+const routes = require("./src/routes");
+const allowedOrigins = [
+  "http://192.168.18.182:5173",
+  "http://localhost:5173",
+  "http://192.168.18.182:3002",
+  "capacitor://localhost",
+  "http://localhost",
+];
 
 class App {
   constructor() {
@@ -18,39 +25,41 @@ class App {
   }
 
   configureMiddleware() {
-    this.app.use(cors({
-      origin: 'http://localhost:5173',
-      credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      allowedHeaders: ['Content-Type', 'Authorization']
-    }));
+    this.app.use(
+      cors({
+        origin: allowedOrigins,
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+      })
+    );
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
-    this.app.use('/public', express.static(path.join(__dirname, 'public')));
- }
+    this.app.use("/public", express.static(path.join(__dirname, "public")));
+  }
 
   configureRoutes() {
     this.app.use((req, res, next) => {
       console.log(`${req.method} ${req.path}`, {
         query: req.query,
-        params: req.params
+        params: req.params,
       });
       next();
     });
-    this.app.use('/api', routes);
+    this.app.use("/api", routes);
   }
 
   async connectToDatabase() {
     try {
       await sequelize.authenticate();
-      console.log('Database connection has been established successfully.');
+      console.log("Database connection has been established successfully.");
       await sequelize.sync();
-      console.log('Models synchronized with database.');
+      console.log("Models synchronized with database.");
       this.app.listen(this.port, () => {
         console.log(`Serveris running on http://localhost:${this.port}`);
       });
     } catch (error) {
-      console.error('Unable to connect to the database:', error);
+      console.error("Unable to connect to the database:", error);
     }
   }
 }
